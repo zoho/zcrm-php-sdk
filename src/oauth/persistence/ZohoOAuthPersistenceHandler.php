628897f0be03c09cc8148aac917a438a070cdf8a
Fabian Bettag <fabian.bettag@tenado.de>
2019-11-12 13:28:15 +0100

    Use prepared Statements for OAuth Persistence

--- src/oauth/persistence/ZohoOAuthPersistenceHandler.php
+++ src/oauth/persistence/ZohoOAuthPersistenceHandler.php
@@ -17,16 +17,25 @@ class ZohoOAuthPersistenceHandler implements ZohoOAuthPersistenceInterface
         try {
             self::deleteOAuthTokens($zohoOAuthTokens->getUserEmailId());
             $db_link = self::getMysqlConnection();
-            $query = "INSERT INTO oauthtokens(useridentifier,accesstoken,refreshtoken,expirytime) VALUES('" . $zohoOAuthTokens->getUserEmailId() . "','" . $zohoOAuthTokens->getAccessToken() . "','" . $zohoOAuthTokens->getRefreshToken() . "'," . $zohoOAuthTokens->getExpiryTime() . ")";
-            
-            $result = mysqli_query($db_link, $query);
+            $stmt = $this->prepareStatement(
+                        $db_link,
+                'INSERT INTO oauthtokens(useridentifier,accesstoken,refreshtoken,expirytime) VALUES(?, ?, ?, ?)',
+                        'sssi',
+                        $zohoOAuthTokens->getUserEmailId(),
+                        $zohoOAuthTokens->getAccessToken(),
+                        $zohoOAuthTokens->getRefreshToken(),
+                        $zohoOAuthTokens->getExpiryTime()
+                    );
+
+            $result = $stmt->execute();
             if (! $result) {
-                Logger::severe("OAuth token insertion failed: (" . $db_link->errno . ") " . $db_link->error);
+                Logger::severe(sprintf('OAuth token insertion failed: (%s) %s', $stmt->errno, $stmt->error));
             }
         } catch (Exception $ex) {
-            Logger::severe("Exception occured while inserting OAuthTokens into DB(file::ZohoOAuthPersistenceHandler)({$ex->getMessage()})\n{$ex}");
+            Logger::severe(sprintf("Exception occured while inserting OAuthTokens into DB(file::ZohoOAuthPersistenceHandler)(%s)\n%s",
+                $ex->getMessage(), $ex));
         } finally {
-            if ($db_link != null) {
+            if ($db_link !== null) {
                 $db_link->close();
             }
         }
@@ -38,13 +47,15 @@ class ZohoOAuthPersistenceHandler implements ZohoOAuthPersistenceInterface
         $oAuthTokens = new ZohoOAuthTokens();
         try {
             $db_link = self::getMysqlConnection();
-            $query = "SELECT * FROM oauthtokens where useridentifier='" . $userEmailId . "'";
-            $resultSet = mysqli_query($db_link, $query);
+            $stmt = $this->prepareStatement($db_link, 'SELECT * FROM oauthtokens where useridentifier = ?', 's', $userEmailId);
+
+            $resultSet = $stmt->execute();
             if (! $resultSet) {
-                Logger::severe("Getting result set failed: (" . $db_link->errno . ") " . $db_link->error);
-                throw new ZohoOAuthException("No Tokens exist for the given user-identifier,Please generate and try again.");
+                Logger::severe(sprintf('Getting result set failed: (%s) %s', $stmt->errno, $stmt->error));
+                throw new ZohoOAuthException('No Tokens exist for the given user-identifier,Please generate and try again.');
             } else {
-                while ($row = mysqli_fetch_row($resultSet)) {
+                $result = $stmt->get_result();
+                while ($result && $row = $result->fetch_row()) {
                     $oAuthTokens->setExpiryTime($row[3]);
                     $oAuthTokens->setRefreshToken($row[2]);
                     $oAuthTokens->setAccessToken($row[1]);
@@ -55,7 +66,7 @@ class ZohoOAuthPersistenceHandler implements ZohoOAuthPersistenceInterface
         } catch (Exception $ex) {
             Logger::severe("Exception occured while getting OAuthTokens from DB(file::ZohoOAuthPersistenceHandler)({$ex->getMessage()})\n{$ex}");
         } finally {
-            if ($db_link != null) {
+            if ($db_link !== null) {
                 $db_link->close();
             }
         }
@@ -67,13 +78,15 @@ class ZohoOAuthPersistenceHandler implements ZohoOAuthPersistenceInterface
         $db_link = null;
         try {
             $db_link = self::getMysqlConnection();
-            $query = "DELETE FROM oauthtokens where useridentifier='" . $userEmailId . "'";
-            $resultSet = mysqli_query($db_link, $query);
+            $stmt = $this->prepareStatement($db_link, 'DELETE FROM oauthtokens where useridentifier= ?', 's', $userEmailId);
+
+            $resultSet = $stmt->execute();
             if (! $resultSet) {
-                Logger::severe("Deleting  oauthtokens failed: (" . $db_link->errno . ") " . $db_link->error);
+                Logger::severe(sprintf('Deleting  oauthtokens failed: (%s) %s', $stmt->errno, $stmt->error));
             }
         } catch (Exception $ex) {
-            Logger::severe("Exception occured while Deleting OAuthTokens from DB(file::ZohoOAuthPersistenceHandler)({$ex->getMessage()})\n{$ex}");
+            Logger::severe(sprintf("Exception occured while Deleting OAuthTokens from DB(file::ZohoOAuthPersistenceHandler)(%s)\n%s",
+                $ex->getMessage(), $ex));
         } finally {
             if ($db_link != null) {
                 $db_link->close();
@@ -89,5 +102,32 @@ class ZohoOAuthPersistenceHandler implements ZohoOAuthPersistenceInterface
             echo "Failed to connect to MySQL: (" . $mysqli_con->connect_errno . ") " . $mysqli_con->connect_error;
         }
         return $mysqli_con;
+    }
+
+    /**
+     * @param \mysqli $db_link
+     * @param string  $query
+     * @param string  $types Parameter Types String
+     * @param mixed   ...$parameters Parameters to be bound
+     *
+     * @throws \InvalidArgumentException Number of arguments does not match type specification
+     * @throws \RuntimeException Statement can not be prepared
+     * @return \mysqli_stmt
+     */
+    private function prepareStatement($db_link, $query, $types, ...$parameters) {
+        if(count($parameters) !== strlen($types)) {
+            throw new \InvalidArgumentException(
+                sprintf('Expected %s parameters, %s given', strlen($types), count($parameters))
+            );
+        }
+
+        $stmt = $db_link->prepare($query);
+        $stmt->bind_param($types, $parameters);
+
+        if(!$stmt) {
+            throw new \RuntimeException('Could not prepare SQL statement', 1573560073);
+        }
+
+        return $stmt;
     }
 }
